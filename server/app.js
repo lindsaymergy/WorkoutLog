fa29a9a78d125b776b16f679693f7c7df67bf8c6
John Mergy <johnmergy@Johns-MacBook-Air.local>
2017-01-30 12:10:34 -0500

    latest commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var app = express();
 
-app.listen(3000, function(){
-	console.log("app is listening on 3000");
-});
+app.use(require("./middleware/headers"));
 
 app.use("/api/test", function(req, res){
 	res.send("Hello World");
 });
+
+app.listen(3000, function(){
+	console.log("app is listening on 3000");
+});
+
